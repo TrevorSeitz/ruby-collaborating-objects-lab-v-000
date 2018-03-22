b6b21fb29a918b3bbf0b28440495e0bc965912f3
Trevor Seitz <trevinla@gmail.com>
2018-03-22 14:03:32 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -13,9 +13,9 @@ class Song
   def self.new_by_filename(filename)
     # binding.pry
     new_song = self.new(filename.split(" - ")[1])
-    binding.pry
-    new_song.artist = filename.split(" - ")[0]
     # binding.pry
+    new_song.artist = filename.split(" - ")[0]
+    binding.pry
     new_song
   end
 
